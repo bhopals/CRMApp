c44a8032c5abca483b34d606b3796a1d6cde0fc4
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-22 20:16:51 -0400

    feat(db): add mongoose connection and body-parser middleware

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = 3000;
+
+//Mongoose Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//Body Parser Setup
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 routes(app);
 
