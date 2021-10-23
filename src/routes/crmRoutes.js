27305bd823681f1f4884fffea898aaaa116efe74
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-22 20:05:40 -0400

    feat(middleware): add middleware init

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,13 @@
 const routes = (app) => {
 
     app.route('/contacts')
-       .get((req, res) => res.send('GET Request Successful'))
+       .get((req, res, next) => {
+         console.log('REQ Method>', req.method)
+         console.log('REQ Url>', req.originalUrl)
+         next()
+       }, (req, res, next) => {
+         res.send('GET Request Successful')
+       })
        .post((req, res) => res.send('POST Request Successful'));
     
     app.route('/contacts/:id')
